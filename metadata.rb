bd0d547972c2feecf4b58b1dbc801e3bbcfc2d73
Ricardo <ricardo.munoz@bq.com>
2015-11-11 12:35:18 +0100

    Added fuse.conf

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer       'Devops BQ'
 maintainer_email 'devops@bq.com'
 license          'All rights reserved'
 description      'Riofs installation to create s3 filesystem'
-version          '0.0.1'
+version          '0.0.2'
 
